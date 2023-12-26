56a636a420a3c0601c683342496748c55e795106
João Gabriel <joaoname9@gmail.com>
2023-12-26 11:10:26 -0300

    refactor: Summary

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -51,7 +51,7 @@ const Summary = () => {
       </div>
       <Button
         onClick={onCheckout}
-        disabled={items.length === 0}
+        disabled={!items.length}
         className="w-full mt-6"
       >
         Pagamento
