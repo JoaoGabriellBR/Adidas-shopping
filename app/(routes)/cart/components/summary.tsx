94eaf494d29d59fa10dd74e9d0aec8c4d6e7a52f
João Gabriel <joaoname9@gmail.com>
2023-12-28 00:11:14 -0300

    refactor: Summary

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -1,60 +1,55 @@
 "use client";
 
-import axios from "axios";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import Button from "@/components/ui/button";
 import useCart from "@/hooks/use-cart";
 import { toast } from "react-hot-toast";
+import { Loader2 } from "lucide-react";
 
 const Summary = () => {
   const searchParams = useSearchParams();
+  const [loading, setLoading] = useState(false);
   const items = useCart((state) => state.items);
   const removeAll = useCart((state) => state.removeAll);
 
-  useEffect(() => {
-    if (searchParams.get("success")) {
-      toast.success("Pagamento completado.");
-      removeAll();
-    }
+  // useEffect(() => {
+  //   if (searchParams.get("success")) {
+  //     toast.success("Pagamento completado.");
+  //     removeAll();
+  //   }
 
-    if (searchParams.get("canceled")) {
-      toast.error("Algo deu errado!");
-    }
-  }, [searchParams, removeAll]);
+  //   if (searchParams.get("canceled")) {
+  //     toast.error("Algo deu errado!");
+  //   }
+  // }, [searchParams, removeAll]);
 
   const totalPrice = items.reduce((total, item) => {
     return total + Number(item.price);
   }, 0);
 
-  console.log(items)
-
   const onCheckout = async () => {
-    const response = await axios.post(
-      `${process.env.NEXT_PUBLIC_API_URL}/checkout`,
-      {
-        productIds: items.map((item) => item.id),
-      }
-    );
-
-    window.location = response.data.url;
-  };
-
-  const checkout = async () => {
-    await fetch("http://localhost:3000/api/checkout", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json"
-      },
-      body: JSON.stringify({ products: items })
-    }).then((response) => {
-      return response.json();
-    }).then((response) => {
-      console.log(response)
-      if (response.url) {
-        window.location.href = response.url;
-      }
-    })
+    setLoading(true);
+    try {
+      await fetch("http://localhost:3000/api/checkout", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify({ products: items })
+      }).then((response) => {
+        return response.json();
+      }).then((response) => {
+        console.log(response)
+        if (response.url) {
+          window.location.href = response.url;
+        }
+      })
+    } catch (e) {
+      console.log(e)
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
@@ -67,12 +62,16 @@ const Summary = () => {
         </div>
       </div>
       <Button
-        // onClick={onCheckout}
-        onClick={checkout}
+        onClick={onCheckout}
         disabled={!items.length}
         className="w-full mt-6"
       >
-        Pagamento
+        {loading ? (
+          <div className="flex flex-row justify-center items-center">
+            <Loader2 className="mr-2 h-6 w-6 animate-spin" />
+            Redirecionando
+          </div>
+        ) : "Pagamento"}
       </Button>
     </div>
   );
